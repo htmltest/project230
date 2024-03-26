5ce6c0beaf06540dc3693536be3320e5fa05f582
htmltest <struev.vs@gmail.com>
2024-03-26 13:03:03 +0300

    updates

--- js/tools.js
+++ js/tools.js
@@ -1,27 +1,56 @@
 $(document).ready(function() {
 
-    $('.partners-section').each(function() {
-        var curSection = $(this);
-        var curSlider = curSection.find('.partners-list');
-        var swiper = new Swiper(curSlider[0], {
-            touchAngle: 30,
-            slidesPerView: 1,
-            breakpoints: {
-                768: {
-                    slidesPerView: 2
-                },
-                1220: {
-                    slidesPerView: 4
-                },
-                1580: {
-                    slidesPerView: 5
-                }
-            },
-            navigation: {
-                nextEl: curSection.find('.partners-section-next')[0],
-                prevEl: curSection.find('.partners-section-prev')[0],
-            },
+    $('.landtv-quality-compare').each(function() {
+        var isMoving = false;
+
+        $('.landtv-quality-compare-sep').on('mousedown touchstart', function() {
+            isMoving = true;
         });
+        $('.landtv-quality-compare').on('mousemove', function(e) {
+            if (isMoving) {
+                var curLeft = e.pageX - $('.landtv-quality-compare').offset().left;
+                var curWidth = $('.landtv-quality-compare').width();
+                var curSize = curLeft / curWidth * 100;
+                if (curSize < 0) {
+                    curSize = 0;
+                }
+                if (curSize > 100) {
+                    curSize = 100;
+                }
+                $('.landtv-quality-compare-sep').css({'left': curSize + '%'});
+                $('.landtv-quality-compare-old').css({'width': curSize + '%'});
+            }
+        });
+        $('.landtv-quality-compare').on('touchmove', function(e) {
+            if (isMoving) {
+                var curLeft = e.originalEvent.touches[0].pageX - $('.landtv-quality-compare').offset().left;
+                var curWidth = $('.landtv-quality-compare').width();
+                var curSize = curLeft / curWidth * 100;
+                if (curSize < 0) {
+                    curSize = 0;
+                }
+                if (curSize > 100) {
+                    curSize = 100;
+                }
+                $('.landtv-quality-compare-sep').css({'left': curSize + '%'});
+                $('.landtv-quality-compare-old').css({'width': curSize + '%'});
+            }
+        });
+        $('.landtv-quality-compare').on('mouseup touchend', function() {
+            isMoving = false;
+        });
+    });
+
+    $('.landtv-welcome-links a').click(function(e) {
+        var curBlock = $($(this).attr('href'));
+        if (curBlock.length == 1) {
+            var curHeaderHeight = 0;
+            if ($('.site-header').length == 1) {
+                curHeaderHeight = $('.site-header').height();
+            }
+            $('html, body').animate({'scrollTop': curBlock.offset().top - curHeaderHeight});
+        }
+        e.preventDefault();
     });
 
 });
